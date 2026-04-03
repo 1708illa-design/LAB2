8b0827895cc33acc046ef482b746316c458c2f8d
Ilia Romaniuk <1708illa@gmail.com>
2026-04-03 14:45:23 +0300

    lab4

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Category, Product, Review
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    # Вимога: видно назву, створено о, оновлено о
+    list_display = ('name', 'created_at', 'updated_at')
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    # Додав ще категорію та ціну, щоб було зрозуміло, що це за товар
+    list_display = ('title', 'category', 'price', 'created_at', 'updated_at')
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('author', 'product', 'created_at', 'updated_at')
--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Назва категорії")
+    # auto_now_add запише час при створенні, auto_now - при кожному оновленні
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Створено о")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Оновлено о")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Категорії"
+
+class Product(models.Model):
+    # Об'єднуємо таблицю Товари з таблицею Категорії
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name="Категорія")
+    title = models.CharField(max_length=200, verbose_name="Назва товару")
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Ціна")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Створено о")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Оновлено о")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = "Товари"
+
+class Review(models.Model):
+    # Об'єднуємо таблицю Відгуки з таблицею Товари
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name="Товар")
+    author = models.CharField(max_length=100, verbose_name="Автор відгуку")
+    text = models.TextField(verbose_name="Текст")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Створено о")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Оновлено о")
+
+    def __str__(self):
+        return f"Відгук від {self.author}"
+
+    class Meta:
+        verbose_name_plural = "Відгуки"
