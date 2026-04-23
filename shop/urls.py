7571eb86f4873b632639420a176e9cb7c71f1adb
Ilia Romaniuk <1708illa@gmail.com>
2026-04-23 17:16:37 +0300

    lab5

--- shop/urls.py
+++ shop/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
+from django.urls import path  # ЦЬОГО РЯДКА У ТЕБЕ НЕ ВИСТАЧАЄ
 from . import views
 
 urlpatterns = [
     path('', views.main_page, name='main'),
+    path('category/<int:category_id>/', views.main_page, name='category_filter'),
     path('page-1/', views.page_one, name='page_1'),
     path('page-2/', views.page_two, name='page_2'),
+    path('contacts/', views.page_tree, name='page_3'),
+    path('cabinet/', views.page_for, name='page_4'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,27 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product, Category
+from django.core.paginator import Paginator
 
-def main_page(request):
-    # Контекст для головної сторінки
-    context = {
-        'page_title': 'Головна',
-        'is_main': True, # Цей прапорець скаже шаблону показати посилання на інші сторінки
-    }
-    return render(request, 'index.html', context)
 
+def main_page(request, category_id=None):
+    categories = Category.objects.all()
+
+    # Фільтрація
+    if category_id:
+        products_list = Product.objects.filter(category_id=category_id)
+    else:
+        products_list = Product.objects.all()
+
+    # Пагінація (по 4 товари)
+    paginator = Paginator(products_list, 9)
+    page_number = request.GET.get('page')
+    products = paginator.get_page(page_number)
+
+    return render(request, 'shop/index.html', {
+        'products': products,
+        'categories': categories,
+    })
+
+
+# Обов'язково додаємо ці функції, щоб не було помилок
 def page_one(request):
-    # Контекст для першої сторінки
-    context = {
-        'page_title': 'Сторінка 1',
-        'is_main': False,
-        'content': 'Це унікальний контент для першої сторінки.',
-    }
-    return render(request, 'index.html', context)
+    categories = Category.objects.all()
+    return render(request, 'shop/page1.html', {'categories': categories})
+
 
 def page_two(request):
-    # Контекст для другої сторінки
-    context = {
-        'page_title': 'Сторінка 2',
-        'is_main': False,
-        'content': 'А це вже текст другої сторінки.',
-    }
-    return render(request, 'index.html', context)
+    categories = Category.objects.all()
+    return render(request, 'shop/page2.html', {'categories': categories})
+def page_tree(request):
+    categories = Category.objects.all()
+    return render(request, 'shop/page3.html', {'categories': categories})
+
+def page_for(request):
+    categories = Category.objects.all()
+    return render(request, 'shop/page4.html', {'categories': categories})
